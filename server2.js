30cdaad91d19d10fae4b16c10b62391a54507b54
guyver64 <guyver64@free.fr>
2025-04-16 21:47:50 +0200

    Update server2.js

--- server2.js
+++ server2.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import serverRoutes from './serverRoutes.js'; // Chemin vers serverRoutes.
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Utilisez les routes de serverRoutes.js
 app.use('/', serverRoutes); // Conservez les routes existantes
 app.get('/custom-route', (req, res) => {
     res.json({ message: 'Route personnalisée de server2.js' });
